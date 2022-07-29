c962b966c3891aeceb61932d82f7a323b734f4f9
gnatunstyles <prokofyev110@gmail.com>
2022-07-29 17:27:05 +0300

    refactored

---
+++ cmd/mocker/main.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/nats-io/stan.go"
+)
+
+const (
+	msg = `{
+		"order_uid": "b563feb7b2b84b6test",
+		"track_number": "WBILMTESTTRACK",
+		"entry": "WBIL",
+		"delivery": {
+		  "name": "Test Testov",
+		  "phone": "+9720000000",
+		  "zip": "2639809",
+		  "city": "Kiryat Mozkin",
+		  "address": "Ploshad Mira 15",
+		  "region": "Kraiot",
+		  "email": "test@gmail.com"
+		},
+		"payment": {
+		  "transaction": "b563feb7b2b84b6test",
+		  "request_id": "321321231",
+		  "currency": "USD",
+		  "provider": "wbpay",
+		  "amount": 1817,
+		  "payment_dt": 1637907727,
+		  "bank": "alpha",
+		  "delivery_cost": 1500,
+		  "goods_total": 317,
+		  "custom_fee": 0
+		},
+		"items": [
+		  {
+			"chrt_id": 9934930,
+			"track_number": "WBILMTESTTRACK",
+			"price": 453,
+			"rid": "ab4219087a764ae0btest",
+			"name": "Snickers",
+			"sale": 123,
+			"size": "0",
+			"total_price": 317,
+			"nm_id": 2389212,
+			"brand": "Chocolate",
+			"status": 202
+		  }
+		],
+		"locale": "en",
+		"internal_signature": "",
+		"customer_id": "test",
+		"delivery_service": "meest",
+		"shardkey": "9",
+		"sm_id": 99,
+		"date_created": "2021-11-26T06:22:19Z",
+		"oof_shard": "1"
+	  }`
+	msg1 = `{
+		"order_uid": "479rhg893fhc480htest",
+		"track_number": "WBILMTESTTRACK",
+		"entry": "WBIL",
+		"delivery": {
+		  "name": "Wipe",
+		  "phone": "+9720000000",
+		  "zip": "2639809",
+		  "city": "Kiryat Mozkin",
+		  "address": "Ploshad Mira 15",
+		  "region": "Kraiot",
+		  "email": "test@gmail.com"
+		},
+		"payment": {
+		  "transaction": "479rhg893fhc480htest",
+		  "request_id": "321321231",
+		  "currency": "USD",
+		  "provider": "wbpay",
+		  "amount": 1817,
+		  "payment_dt": 1637907727,
+		  "bank": "alpha",
+		  "delivery_cost": 1500,
+		  "goods_total": 317,
+		  "custom_fee": 0
+		},
+		"items": [
+		  {
+			"chrt_id": 9934930,
+			"track_number": "WBILMTESTTRACK",
+			"price": 453,
+			"rid": "ab4219087a764ae0btest",
+			"name": "kekd1",
+			"sale": 30,
+			"size": "0",
+			"total_price": 317,
+			"nm_id": 2389212,
+			"brand": "Vivienne Sabo",
+			"status": 202
+		  }
+		],
+		"locale": "en",
+		"internal_signature": "",
+		"customer_id": "test",
+		"delivery_service": "meest",
+		"shardkey": "9",
+		"sm_id": 99,
+		"date_created": "2021-11-26T06:22:19Z",
+		"oof_shard": "1"
+	  }`
+	msg3 = `{
+		"order_uid": "111b563feb7b2b84b6test",
+		"track_number": "WBILMTESTTRACK",
+		"entry": "WBIL",
+		"delivery": {
+			"name": "John Doe",
+			"phone": "+79112345678",
+			"zip": "188505",
+			"city": "Annino",
+			"address": "10 Pyatiletki 15",
+			"region": "Leningradskaya Oblast",
+			"email": "aew@gmail.com"
+		},
+		"payment": {
+		  "transaction": "111b563feb7b2b84b6test",
+		  "request_id": "321321231",
+		  "currency": "EUR",
+		  "provider": "wbpay",
+		  "amount": 1817,
+		  "payment_dt": 1637907727,
+		  "bank": "alpha",
+		  "delivery_cost": 1500,
+		  "goods_total": 317,
+		  "custom_fee": 0
+		},
+		"items": [
+		  {
+			"chrt_id": 9934930,
+			"track_number": "WBILMTESTTRACK",
+			"price": 453,
+			"rid": "ab4219087a764ae0btest",
+			"name": "Air Force 1",
+			"sale": 30,
+			"size": "0",
+			"total_price": 317,
+			"nm_id": 2389212,
+			"brand": "Vivienne Sabo",
+			"status": 202
+		  },
+		  {
+			"chrt_id": 312,
+			"track_number": "WBILMTESTTRACK",
+			"price": 453,
+			"rid": "ab4219087a764ae0btest",
+			"name": "Air Force 1",
+			"sale": 30,
+			"size": "0",
+			"total_price": 317,
+			"nm_id": 2389212,
+			"brand": "Vivienne Sabo",
+			"status": 202
+		  }
+		],
+		"locale": "en",
+		"internal_signature": "",
+		"customer_id": "test",
+		"delivery_service": "meest",
+		"shardkey": "9",
+		"sm_id": 99,
+		"date_created": "2021-11-26T06:22:19Z",
+		"oof_shard": "1"
+	  }`
+)
+
+func main() {
+	sc, err := stan.Connect("test-cluster", "event-sender", stan.NatsURL(stan.DefaultNatsURL))
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer sc.Close()
+
+	err = sc.Publish("channel", []byte(msg3))
+	if err != nil {
+		log.Fatalf("Error through message publishing: %s", err)
+	}
+	fmt.Println("Message send sucessful.")
+
+}
